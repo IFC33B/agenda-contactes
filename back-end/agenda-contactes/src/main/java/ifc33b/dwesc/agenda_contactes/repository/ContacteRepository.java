9941d0724c22ab7ef734c93a5a703f6cd06697c0
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-22 16:00:01 +0100

    Add(ContacteService): add new query

--- back-end/agenda-contactes/src/main/java/ifc33b/dwesc/agenda_contactes/repository/ContacteRepository.java
+++ back-end/agenda-contactes/src/main/java/ifc33b/dwesc/agenda_contactes/repository/ContacteRepository.java
@@ -1,9 +1,14 @@
 package ifc33b.dwesc.agenda_contactes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ifc33b.dwesc.agenda_contactes.model.Contacte;
 
+
 @Repository
-public interface ContacteRepository extends JpaRepository<Contacte, Long> {}
+public interface ContacteRepository extends JpaRepository<Contacte, Long> {
+    List<Contacte> findByNom(String nom);
+}
