11dd9fc134563d1663341591116ea899e1232ec9
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-22 15:15:59 +0100

    Add - base structure for the backend

---
+++ back-end/agenda-contactes/src/test/java/ifc33b/dwesc/agenda_contactes/AgendaContactesApplicationTests.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.agenda_contactes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContactesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
