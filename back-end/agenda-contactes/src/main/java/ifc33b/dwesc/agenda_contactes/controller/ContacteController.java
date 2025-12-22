093e1997f203ca11286cb256e4ed684db7767b33
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-22 16:14:56 +0100

    Add(ContacteController): method to return user

--- back-end/agenda-contactes/src/main/java/ifc33b/dwesc/agenda_contactes/controller/ContacteController.java
+++ back-end/agenda-contactes/src/main/java/ifc33b/dwesc/agenda_contactes/controller/ContacteController.java
@@ -1,12 +1,21 @@
 package ifc33b.dwesc.agenda_contactes.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import ifc33b.dwesc.agenda_contactes.dto.ContacteRequest;
+import ifc33b.dwesc.agenda_contactes.dto.ContacteResponse;
+import ifc33b.dwesc.agenda_contactes.service.ContacteService;
+
 
 
 
@@ -14,9 +23,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/contactes")
 public class ContacteController {
+    @Autowired
+    ContacteService contacteService;
+
     @GetMapping("/{nom}") // Devolver un contacto por su nombre
-    public int getContacte() {
-        return 0;
+    public ResponseEntity<List<ContacteResponse>> getContacte(@RequestBody ContacteRequest contacteRequest, @PathVariable String nom) {
+        // Service
+        List<ContacteResponse> contactes = contacteService.getContacte(contacteRequest);
+
+        // HTTP response
+        return ResponseEntity.ok(contactes);
     }
     
     @PostMapping() // Crear un contacto
