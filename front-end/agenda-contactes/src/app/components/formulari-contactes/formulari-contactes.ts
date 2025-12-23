7d7c931f1905104468acc031aa419334d5aaa260
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 15:18:31 +0100

    Add: base structure for the frontend

---
+++ front-end/agenda-contactes/src/app/components/formulari-contactes/formulari-contactes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulari-contactes',
+  imports: [],
+  templateUrl: './formulari-contactes.html',
+  styleUrl: './formulari-contactes.css',
+})
+export class FormulariContactes {
+
+}
---
+++ front-end/agenda-contactes/src/app/components/llista-contactes/llista-contactes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-llista-contactes',
+  imports: [],
+  templateUrl: './llista-contactes.html',
+  styleUrl: './llista-contactes.css',
+})
+export class LlistaContactes {
+
+}
