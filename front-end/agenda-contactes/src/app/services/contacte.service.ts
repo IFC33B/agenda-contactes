360b901c68db88f31e5594d1d5ba6df807745161
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 15:27:19 +0100

    Create(contacte.service) base structure

--- front-end/agenda-contactes/src/app/services/contacte.service.ts
+++ front-end/agenda-contactes/src/app/services/contacte.service.ts
@@ -1,8 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContacteService {
-  
+  private apiURL = 'http://localhost:8080/api/contactes';
+
+  constructor(private http: HttpClient) {};
 }
