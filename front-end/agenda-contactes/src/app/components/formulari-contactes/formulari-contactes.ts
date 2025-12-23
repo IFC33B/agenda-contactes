de1f510a7fef3fe7eab616c3b7fd2cea92a1ce66
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 16:50:17 +0100

    Add(formulari-contactes.ts): afegirContactes() method

--- front-end/agenda-contactes/src/app/components/formulari-contactes/formulari-contactes.ts
+++ front-end/agenda-contactes/src/app/components/formulari-contactes/formulari-contactes.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ContacteService } from '../../services/contacte.service';
+import { ContacteRequest } from '../../models';
 
 @Component({
   selector: 'app-formulari-contactes',
@@ -7,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './formulari-contactes.css',
 })
 export class FormulariContactes {
+  carregant = signal(false);
+  error = signal<string | null>(null);
 
+  // datos del formulari
+  nom: string = '';
+  telefon: string = '';
+  email: string = '';
+
+  constructor(private contacteService: ContacteService) { }
+
+  // Añadir contacto
+  afegirContactes() {
+    this.carregant.set(true);
+    this.error.set(null);
+
+    // Request
+   const contacteRequest: ContacteRequest = {nom: this.nom, telefon: this.telefon, email: this.email};
+
+    // Service
+    this.contacteService.createContacte(contacteRequest).subscribe({
+      next: (dades) => {
+        this.carregant.set(false)
+      },
+
+      error: (err) => {
+        this.error.set('Error al añadir un contacto')
+        this.carregant.set(false);
+        console.log(err);
+      }
+    })
+  }
 }
