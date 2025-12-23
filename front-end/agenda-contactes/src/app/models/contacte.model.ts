20811ef05bed275b27a84aeaa6468ba1dc5cef6b
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 15:25:26 +0100

    Create(contacte.model, index.ts): interfaces

--- front-end/agenda-contactes/src/app/models/contacte.model.ts
+++ front-end/agenda-contactes/src/app/models/contacte.model.ts
@@ -1,2 +1,12 @@
-export class ContacteModel {
+export interface Contacte {
+    id: string;
+    nom: string;
+    telefon: string;
+    email: string;
+}
+
+export interface ContacteRequest {
+    nom: string;
+    telefon: string;
+    email: string;
 }
