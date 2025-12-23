1bd571b276deb0165d57b20c2fb56639594337cc
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 15:33:50 +0100

    Add(contacte.service) handleError method

--- front-end/agenda-contactes/src/app/services/contacte.service.ts
+++ front-end/agenda-contactes/src/app/services/contacte.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Contacte } from '../models';
@@ -15,7 +15,23 @@ export class ContacteService {
   getContacte(id: number): Observable<Contacte> {
     return this.http.get<Contacte>(`${this.apiURL}/${id}`)
       .pipe(
-        catchError(throwError)
+        catchError(this.handleError)
       )
+  }
+
+  // Gestión de errores
+    private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Error desconocido';
+
+    if (error.error instanceof ErrorEvent) {
+      // Error del cliente
+      errorMessage = `Error: ${error.error.message}`
+    } else {
+      // Error del servidor
+      errorMessage = `Codigo de error: ${error.status}, Mensaje: ${error.message}`;
+    }
+
+    console.log(errorMessage);
+    return throwError(() => Error(errorMessage))
   }
 }
