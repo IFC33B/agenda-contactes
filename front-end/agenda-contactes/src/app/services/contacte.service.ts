d0fc7b2b06cc7a26dd1922a9d46b6f8739c849a9
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 15:32:30 +0100

    Add(contacte.service): add getContacte method

--- front-end/agenda-contactes/src/app/services/contacte.service.ts
+++ front-end/agenda-contactes/src/app/services/contacte.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Contacte } from '../models';
 
 @Injectable({
   providedIn: 'root',
@@ -8,4 +10,12 @@ export class ContacteService {
   private apiURL = 'http://localhost:8080/api/contactes';
 
   constructor(private http: HttpClient) {};
+
+  // Obtener un contacto
+  getContacte(id: number): Observable<Contacte> {
+    return this.http.get<Contacte>(`${this.apiURL}/${id}`)
+      .pipe(
+        catchError(throwError)
+      )
+  }
 }
