5f1d40db55c3fe1b6c455c5ff9a75f4a6577ffac
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-23 15:41:06 +0100

    Add(llista-contactes.ts) base structure

--- front-end/agenda-contactes/src/app/components/llista-contactes/llista-contactes.ts
+++ front-end/agenda-contactes/src/app/components/llista-contactes/llista-contactes.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Contacte } from '../../models';
+import { ContacteService } from '../../services/contacte.service';
 
 @Component({
   selector: 'app-llista-contactes',
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './llista-contactes.css',
 })
 export class LlistaContactes {
+  missatges = signal<Contacte[]>([]);
+  carregant = signal(true);
+  error = signal<string | null>(null);
+
+  constructor(private contacteService: ContacteService) {}
 
 }
