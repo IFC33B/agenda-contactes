d0ec75e82ea31b44fa694cdf96a2ce236f79a8e0
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-07 14:19:20 +0100

    Crear la entidad contacte.java y sus atributos

---
+++ back-end/agenda-contactes/src/main/java/daw/dwes/agenda_contactes/model/Contacte.java
@@ -0,0 +1,27 @@
+package daw.dwes.agenda_contactes.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "contactes")
+public class Contacte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false)
+    private int telefon;
+
+    @Column(nullable = false)
+    private String email;
+}
