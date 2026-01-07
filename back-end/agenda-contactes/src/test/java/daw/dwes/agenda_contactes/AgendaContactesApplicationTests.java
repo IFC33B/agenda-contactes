724ef2121a99222e1d0fddc4faf5e7ca317dd2b1
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-07 14:02:03 +0100

    Archivos iniciales de SpringBoot

---
+++ back-end/agenda-contactes/src/test/java/daw/dwes/agenda_contactes/AgendaContactesApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.dwes.agenda_contactes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContactesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
